cbf976362090908c577cf2ba1e5a0361517621a4
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2023-03-05 17:33:57 +0100

    Add initial project

---
+++ BeThere/BeThere/BeThereApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct BeThereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeThere/BeThereTests/BeThereTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import BeThere
+
+final class BeThereTests: XCTestCase {
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+}
