ce459441c19d097cda55f132b9d9485d14befeb0
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2023-03-17 11:43:06 +0100

    Setup use of Resolver

--- BeThere/BeThere/App/BeThereApp.swift
+++ BeThere/BeThere/App/BeThereApp.swift
@@ -1,12 +1,10 @@
+import Resolver
 import SwiftUI
 
 @main
 struct BeThereApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    @StateObject var coordinator = AppCoordinator(
-        authenticationCoordinator: AuthenticationCoordinator(root: .login),
-        contentCoordinator: ContentCoordinator(root: .main)
-    )
+    @StateObject var coordinator: AppCoordinator = Resolver.resolve()
 
     var body: some Scene {
         WindowGroup {
