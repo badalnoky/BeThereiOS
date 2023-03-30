964923ac1595f3115bf493db06004df09a509c4c
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2023-03-30 19:19:27 +0200

    Add used colors

---
+++ BeThere/BeThere/Resources/Generated/Colors.swift
@@ -0,0 +1,23 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+// Template by Bertalan Dálnoky
+
+import SwiftUI
+
+public extension Color {
+   static let accentColor = Color("AccentColor")
+
+   static let background = Color("Background")
+
+   static let error = Color("Error")
+
+   static let primary = Color("Primary")
+
+   static let secondary = Color("Secondary")
+
+   static let success = Color("Success")
+
+   static let text = Color("Text")
+
+}
+
