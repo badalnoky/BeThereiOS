5d3956cae60522dee62135b8151c12c9fcb2e297
Bertalan Dálnoky <dalnoky.bertalan.andras@gmail.com>
2023-04-14 17:43:50 +0200

    Add deletion of past events

--- BeThere/BaseKit/BaseKit/Extensions/Double+Extensions.swift
+++ BeThere/BaseKit/BaseKit/Extensions/Double+Extensions.swift
@@ -3,4 +3,5 @@ public extension Double {
     static let one: Double = 1
     static let smallImageSize: Double = 20
     static let defaultImageSize: Double = 100
+    static let day: Double = 60 * 60 * 24
 }
